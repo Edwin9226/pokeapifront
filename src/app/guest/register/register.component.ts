ad03b103ef490d8dd94018345f6b16dcccd8e54c
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-10-24 15:53:54 -0500

    implement componente del registro

--- src/app/guest/register/register.component.ts
+++ src/app/guest/register/register.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { User } from 'src/app/models/user.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  faUser = faUserCircle;
+  errorMessage: string= "";
+
+  constructor(private authenticationServices: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
+    if(this.authenticationServices.currentUserValue?.id){
+      this.router.navigate(['/profile']);
+    }
   }
 
+  register(){
+    this.authenticationServices.register(this.user).subscribe(data=>{
+      this.router.navigate(['/login']);
+    });
+  }
 }
